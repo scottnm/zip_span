aa423bf4241c4bc9418d01d24fb201433fc0fd74
Scott Munro <scottnmunro@gmail.com>
2020-01-08 22:16:41 -0800

    stub zipspan to check for syntax

--- main.cpp
+++ main.cpp
@@ -2,28 +2,14 @@
 
 typedef const char* cstr;
 
-void test_function(gsl::span<int> i)
-{
-    for (int v : i)
-    {
-        printf("%i\n", v);
-    }
-}
-
 void main()
 {
     cstr keys[] = { "key1", "key2", "key3" };
     int values[] = { 1, 2, 3, 4 };
     char aux_data[] = { '1', '2', '3', '4', '5' };
 
-    /* FIXME:
     for (auto nextTuple : zip_span(keys, values, aux_data))
     {
-        printf("%s=%i\t|%c", nextTuple<0>.get(), nextTuple<1>.get(), nextTuple<2>.get());
+        printf("%s=%i\t|%c", std::get<0>(nextTuple), std::get<1>(nextTuple), std::get<2>(nextTuple));
     }
-    */
-    (void)keys;
-    (void)values;
-    test_function(values);
-    (void)aux_data;
 }
--- span_zip.h
+++ span_zip.h
@@ -1,3 +1,18 @@
 #pragma once
 
 #include <gsl/span>
+
+// FIXME: mocked out what the return value is actually supposed to be
+#include <vector>
+std::vector<std::tuple<const char*, int, char>>
+zip_span(
+    // FIXME: mocked out what the input params are
+    // should be made more generic
+    gsl::span<const char*> keys,
+    gsl::span<int> values,
+    gsl::span<char> aux
+    )
+{
+    (void)keys;(void)values;(void)aux;
+    return {};
+}
