e08dd8fb054a5a4e5e365f6593f57873ffb85134
Scott Munro <scottnmunro@gmail.com>
2020-01-08 22:26:04 -0800

    verify that copy-less reference tuples work

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 typedef const char* cstr;
 
+template<typename T>
+struct NonCopy
+{
+    T v;
+
+    // Delete copy assn and copy ctor
+    NonCopy(T v) : v(v) {}
+    NonCopy(const NonCopy&)=delete;
+    NonCopy& operator=(const NonCopy&)=delete;
+};
+
 #include "span_zip.h"
 
 void main()
 {
-    cstr keys[] = { "key1", "key2", "key3" };
-    int values[] = { 1, 2, 3, 4 };
-    char aux_data[] = { '1', '2', '3', '4', '5' };
+    NonCopy<cstr> keys[] = { "key1", "key2", "key3" };
+    NonCopy<int> values[] = { 1, 2, 3, 4 };
+    NonCopy<char> aux_data[] = { '1', '2', '3', '4', '5' };
 
     for (auto nextTuple : zip_span(keys, values, aux_data))
     {
-        printf("%s=%i\t|%c", std::get<0>(nextTuple), std::get<1>(nextTuple), std::get<2>(nextTuple));
+        printf("%s=%i\t|%c", std::get<0>(nextTuple).v, std::get<1>(nextTuple).v, std::get<2>(nextTuple).v);
     }
 }
--- span_zip.h
+++ span_zip.h
@@ -4,15 +4,22 @@
 
 // FIXME: mocked out what the return value is actually supposed to be
 #include <vector>
-std::vector<std::tuple<cstr&, int&, char&>>
+std::vector<std::tuple<NonCopy<cstr>&, NonCopy<int>&, NonCopy<char>&>>
 zip_span(
     // FIXME: mocked out what the input params are
     // should be made more generic
-    gsl::span<const char*> keys,
-    gsl::span<int> values,
-    gsl::span<char> aux
+    gsl::span<NonCopy<cstr>> keys,
+    gsl::span<NonCopy<int>> values,
+    gsl::span<NonCopy<char>> aux
     )
 {
     (void)keys;(void)values;(void)aux;
+    return {};
+}
+
+std::vector<std::tuple<NonCopy<cstr>, NonCopy<int>, NonCopy<char>>>
+test_no_copy(
+    )
+{
     return {};
 }
